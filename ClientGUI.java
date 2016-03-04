0733a3e186085375e757048d299d53a6264fb0c7
MattMcG1 <mcgranahanmatthew@gmail.com>
2016-03-04 12:10:29 -0800

    Added files via upload

---
+++ ClientGUI.java
@@ -0,0 +1,57 @@
+ import javafx.application.Application;
+ import javafx.geometry.Rectangle2D;
+ import javafx.scene.Group;
+ import javafx.scene.Scene; 
+ import javafx.scene.image.Image;
+ import javafx.scene.image.ImageView;
+ import javafx.scene.layout.HBox;
+ import javafx.scene.paint.Color;
+ import javafx.stage.Stage; 
+ import javafx.scene.layout.HBox;
+ import javafx.stage.PopupWindow;
+ import java.util.ArrayList;
+ 
+ public UnoCard pile;
+ public ArrayList<UnoCard> hand;
+
+
+public class ClientGUI extends Application {
+
+	HBox handBox = new HBox(5);
+	
+	public void start(Stage primaryStage) {
+
+	}
+	
+	public void update() {
+		refreshHand();
+		refreshPile();
+	}
+	
+	public Integer proposeColor() {
+		return 0;
+	}
+	
+	public String getFileName(UnoCard card) {
+		String col = card.color.name();
+		String act = card.action.name();
+		String num = Integer.toString(card.number);
+		String fileName = col + act + num;
+		return fileName;
+	}
+	
+	public void refreshHand() {
+		handBox.getChildren().clear();
+		for(int i = 0; i < hand.size(); i++) {
+			String fileName = getFileName(hand.get(i));
+			Image cardImage = new Image("/image/" + fileName + ".png");
+			ImageView viewCard = new ImageView(cardImage);
+			handBox.getChildren().add(viewCard);
+		}
+	}
+	
+	public void refreshPile() {
+
+	}
+}
+
