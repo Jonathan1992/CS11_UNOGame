86a0afd735bc406d10f2ea21df0ed325703b1105
Jonathan1992 <yansong.liu@hotmail.com>
2016-03-12 03:53:55 +0800

    GUI Update

--- UNOPlayer.java
+++ UNOPlayer.java
@@ -1,12 +1,6 @@
 import java.io.IOException;
 import java.util.*;
 
-/**
- * This class defines the methods and variables of a player
- * @author Yansong Liu, Matthew McGranahan
- *
- */
-
 public class UNOPlayer {
   ArrayList<UNOCard> hand = new ArrayList<UNOCard>();
   CardDealer dealer;
@@ -14,10 +8,6 @@ public class UNOPlayer {
   
   UNOServer.PlayerSock sock;
   
-   /**
-   * Sets the CardDealer that will be used for this class
-   * @param dl The CardDealer class for this game
-   */
   public void setDealer(CardDealer dl) {
     this.dealer = dl;
     // Draw 8 cards
@@ -27,74 +17,37 @@ public class UNOPlayer {
     sortHand();
   }
   
-  /**
-   * Constructor for the UNOPlayer class
-   * @param sock The UNOServer connection being used
-   */
   UNOPlayer(UNOServer.PlayerSock sock) {
     this.sock = sock;
   }
   
-  /**
-   * Gets the number of cards in a hand
-   * @return int The size of a player's hand
-   */
   public int getNumOfCard() {
     return hand.size();
   }
   
-  /**
-   * Adds a card to the player's hand
-   * @param card The UNOCard to add to hand
-   * @return void
-   */
+  
   public void addCard(UNOCard card) {
     hand.add(card);
   }
   
-  /**
-   * Plays the card the player clicks on in their hand
-   * @param index int The position of the card to be played
-   * @return UNOCard The UNOCard at the index
-   */
   public UNOCard playCard(int index) {
     return hand.remove(index);
   }
   
-  /**
-   * Gets a card in the player's hand
-   * @param index int The index in the hand of the card
-   * @return UNOCard The card at the index
-   */
   public UNOCard getCard(int index) {
     return hand.get(index);
   }
   
-  /**
-   * Draws cards because of an action card
-   * @param num int The number of cards a player has to draw
-   * @return void
-   */
   public void forceDraw(int num) {
     for (int i=0; i<num; i++) {
       hand.add(dealer.drawCard());
     }
   }
   
-  /**
-   * Gets the nickanme of the player
-   * @return String the nickname of the client
-   */
   public String getNickName() {
     return this.nickName;
   }
   
-  /**
-   * Gets the index of a card clicked on to be played
-   * @return int The index of the card to be played
-   * @throws IOException
-   * @throws ClassNotFoundException
-   */
   public int proposeCard() throws IOException, ClassNotFoundException {
     // Propose the card over the network
     UNOMessage msg = new UNOMessage();
@@ -121,12 +74,6 @@ public class UNOPlayer {
     return index;
   }
 
-  /**
-   * Shows the number of cards in this player's hand to the others
-   * @param pile The last card in play
-   * @param counts The ArratList<Integer> of the playerID of each player
-   * @throws IOException
-   */
   public void broadCastHand(UNOCard pile,
       ArrayList<Integer> counts,
       ArrayList<String> names) throws IOException {
@@ -142,10 +89,6 @@ public class UNOPlayer {
     System.out.println("Hand Broadcasted!");
   }
   
-  /**
-   * Gives each player the result of the previous game
-   * @throws IOException
-   */
   public void broadCastResult() throws IOException {
     UNOMessage msg = new UNOMessage();
     
@@ -161,9 +104,6 @@ public class UNOPlayer {
     System.out.println("Result Broadcasted!");
   }
   
-   /**
-   * Sorts the player's hand
-   */
   public void sortHand() {
     Collections.sort(hand);
   }
--- test.java
+++ test.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class test {
 
   public static void main(String[] args) throws IOException {
-    ProcessBuilder pb = new ProcessBuilder("UNOServer");
+    ProcessBuilder pb = new ProcessBuilder("java", "UNOServer");
     Process p = pb.start();
   }
 
