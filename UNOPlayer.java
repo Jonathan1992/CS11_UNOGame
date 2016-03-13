5cd90fa1c46fe93105d01748462495f23647c637
Jonathan1992 <yansong.liu@hotmail.com>
2016-03-13 09:39:40 +0800

    Update UNOPlayer.java

--- UNOPlayer.java
+++ UNOPlayer.java
@@ -1,6 +1,11 @@
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * This class is for implementing the UNO Player
+ * Including maintaining the client-server socket for communication
+ * @author Yansong Liu, Matthew McGranahan
+ */
 public class UNOPlayer {
   ArrayList<UNOCard> hand = new ArrayList<UNOCard>();
   CardDealer dealer;
@@ -8,6 +13,10 @@ public class UNOPlayer {
   
   UNOServer.PlayerSock sock;
   
+  /**
+   * Set the dealer for this player
+   * @param dl the Card Dealer for this player
+   */
   public void setDealer(CardDealer dl) {
     this.dealer = dl;
     // Draw 8 cards
@@ -17,37 +26,69 @@ public class UNOPlayer {
     sortHand();
   }
   
+  /**
+   * Constructor for the UNO Player
+   * @param sock the client-server player socket class
+   */
   UNOPlayer(UNOServer.PlayerSock sock) {
     this.sock = sock;
   }
   
+  /**
+   * Get the number of cards for this player
+   * @return int the number of cards for this player
+   */
   public int getNumOfCard() {
     return hand.size();
   }
   
-  
+  /**
+   * Add a card to this player's hand
+   * @param card UNO Card to add
+   */
   public void addCard(UNOCard card) {
     hand.add(card);
   }
   
+  /**
+   * Player a card
+   * @param index index of the card in player's hand
+   * @return UNOCard the UNO Card played
+   */
   public UNOCard playCard(int index) {
     return hand.remove(index);
   }
   
+  /**
+   * Get a card in hand
+   * @param index index of the card in player's hand
+   */
   public UNOCard getCard(int index) {
     return hand.get(index);
   }
   
+  /**
+   * Force this player to draw card
+   * @param num number of cards of the force drawing
+   */
   public void forceDraw(int num) {
     for (int i=0; i<num; i++) {
       hand.add(dealer.drawCard());
     }
   }
   
+  /**
+   * Get the nickname of this player
+   * @return String player's name
+   */
   public String getNickName() {
     return this.nickName;
   }
   
+  /**
+   * Propose the player over network to enter a choice
+   * @return int the index return by client
+   */
   public int proposeCard() throws IOException, ClassNotFoundException {
     // Propose the card over the network
     UNOMessage msg = new UNOMessage();
@@ -73,7 +114,13 @@ public class UNOPlayer {
     
     return index;
   }
-
+  
+  /**
+   * Broadcast information to this player
+   * @param pile pile card
+   * @param counts number of cards for all players in the game
+   * @param names nicknames for all players in the game
+   */
   public void broadCastHand(UNOCard pile,
       ArrayList<Integer> counts,
       ArrayList<String> names) throws IOException {
@@ -89,6 +136,9 @@ public class UNOPlayer {
     System.out.println("Hand Broadcasted!");
   }
   
+  /**
+   * Broadcast the game result
+   */
   public void broadCastResult() throws IOException {
     UNOMessage msg = new UNOMessage();
     
@@ -104,6 +154,9 @@ public class UNOPlayer {
     System.out.println("Result Broadcasted!");
   }
   
+  /**
+   * Sort the player's hand of cards
+   */
   public void sortHand() {
     Collections.sort(hand);
   }
